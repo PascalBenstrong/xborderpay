7b2375f61318afcd19d1f15a6efa157157226489
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-06-07 00:09:10 +0200

    created custom stepper

--- app/components/stepper.tsx
+++ app/components/stepper.tsx
@@ -8,6 +8,7 @@ import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import ETransferSuccess from "./e-transfer/success";
+import SelectAccountPayee from "./e-transfer/selectPayee";
 
 const steps = [
   {
@@ -53,26 +54,26 @@ export default function VerticalLinearStepper() {
   return (
     <Box>
       <Grid container spacing={1}>
-        <Grid xs={12} md={3}>
-          <Stepper activeStep={activeStep} orientation="vertical">
+        <Grid xs={12} lg={2}>
+          <Stepper activeStep={activeStep} orientation="vertical" sx={{display: {xs: "none", lg: "flex"}}}>
             {steps.map((step, index) => (
               <Step key={step.label}>
-                <StepLabel
-                  optional={
-                    index === 2 ? (
-                      <Typography variant="caption">Last step</Typography>
-                    ) : null
-                  }
-                >
-                  {step.label}
-                </StepLabel>
+                <StepLabel>{step.label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          <Stepper activeStep={activeStep} orientation="horizontal" sx={{display: {xs: "flex", lg: "none"}, mb: 3}}>
+            {steps.map((step, index) => (
+              <Step key={step.label}>
+                <StepLabel>{step.label}</StepLabel>
               </Step>
             ))}
           </Stepper>
         </Grid>
-        <Grid xs={12} md={9}>
-          
-          {activeStep <= steps.length-2 && <Box sx={{ mb: 2 }}>
+        <Grid xs={12} md={12} lg={10}>
+          {activeStep == 0 && <SelectAccountPayee />}
+          {activeStep <= steps.length - 2 && (
+            <Box sx={{ mb: 2 }}>
               <Button
                 variant="outlined"
                 onClick={handleBack}
@@ -87,8 +88,9 @@ export default function VerticalLinearStepper() {
               >
                 {activeStep === steps.length - 2 ? "Send Money" : "NEXT"}
               </Button>
-          </Box>}
-          {activeStep === steps.length-1 && <ETransferSuccess />}
+            </Box>
+          )}
+          {activeStep === steps.length - 1 && <ETransferSuccess />}
         </Grid>
       </Grid>
     </Box>
