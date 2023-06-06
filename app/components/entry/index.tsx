eca1b7ff1bc87e239043928f96e924a11412fb97
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-06-07 00:07:59 +0200

    created custom entry

---
+++ app/components/entry/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { alpha, styled } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import TextField, { TextFieldProps } from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import { OutlinedInputProps } from '@mui/material/OutlinedInput';
+
+export const ValidationTextField = styled(TextField)({
+  '& input:valid + fieldset': {
+    borderColor: 'white',
+    borderWidth: 2,
+  },
+  '& input:invalid + fieldset': {
+    borderColor: 'red',
+    borderWidth: 2,
+  },
+  '& input:valid:focus + fieldset': {
+    borderLeftWidth: 6,
+    padding: '4px !important', // override inline-style
+  },
+});
