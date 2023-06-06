06749404a5ce592b5d39d30041f846cb7de17e81
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-06-06 14:41:01 +0200

    connected transactions page to api

--- app/(account)/transactions/page.tsx
+++ app/(account)/transactions/page.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+"use client";
+import { Container, Paper } from "@mui/material";
+import React from "react";
+import { useFetcher } from "../../utils";
+import TransactionCard from "../../components/transaction_card";
+import Title from "../../components/title";
+
+function GetData() {
+  const { data, isError, isLoading } = useFetcher(`/api/transactions`);
+
+  return {
+    transactions: data?.data,
+    isLoading,
+    isError,
+  };
+}
 
 export default function TransactionsPage() {
+  const { transactions, isError, isLoading } = GetData();
+
   return (
-    <div>Transactions Page</div>
-  )
+    <Container maxWidth="lg" sx={{ pt: 15, px: 200 }}>
+      <Title title="Activities" />
+      <Paper
+        sx={{
+          bgcolor: "secondary.main",
+          p: 2,
+          pb: 4,
+          maxHeight: 600,
+          borderRadius: 3,
+          overflow: "auto",
+        }}
+      >
+        {transactions &&
+          transactions.map((item: any, index: number) => (
+            <TransactionCard
+              key={index}
+              to={item.to}
+              type={item.type}
+              currency={item.currency}
+              amount={item.amount}
+              wallet={item.wallet}
+              timestamp={item.timestamp}
+            />
+          ))}
+      </Paper>
+    </Container>
+  );
 }
