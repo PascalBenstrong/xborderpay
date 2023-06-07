e211c1ffdda2eb9898c5db07532767d263a015c9
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-06-07 10:21:37 +0200

    added handling if reseting the form

--- app/(account)/e-transfer/page.tsx
+++ app/(account)/e-transfer/page.tsx
@@ -1,13 +1,57 @@
 "use client";
-import { Container, Paper } from '@mui/material'
-import React from 'react'
-import TransactionCard from '../../components/transaction_card'
-import Title from '../../components/title'
-import VerticalLinearStepper from '../../components/stepper';
+import { Container, Paper,Unstable_Grid2 as Grid, Box, Button, Step, StepLabel, Stepper, Typography } from "@mui/material";
+import React from "react";
+import TransactionCard from "../../components/transaction_card";
+import Title from "../../components/title";
+import VerticalLinearStepper from "../../components/stepper";
+import EnterAmount from "../../components/e-transfer/enterAmount";
+import Review from "../../components/e-transfer/review";
+import SelectAccountPayee from "../../components/e-transfer/selectPayee";
+import ETransferSuccess from "../../components/e-transfer/success";
+import UserInfoCard from "../../components/e-transfer/userInfoCard";
+
+const steps = [
+  {
+    label: "Account & Payee",
+    description: `For each ad campaign that you create, you can control how much
+              you're willing to spend on clicks and conversions, which networks
+              and geographical locations you want your ads to show on, and more.`,
+  },
+  {
+    label: "Amount",
+    description:
+      "An ad group contains one or more ads which target a shared set of keywords.",
+  },
+  {
+    label: "Preview",
+    description:
+      "An ad group contains one or more ads which target a shared set of keywords.",
+  },
+  {
+    label: "Successful",
+    description: `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`,
+  },
+];
 
 export default function ETransferPage() {
+  const [activeStep, setActiveStep] = React.useState(2);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
   return (
-    <Container maxWidth="lg" sx={{ pt: 15, }}>
+    <Container maxWidth="lg" sx={{ pt: 15 }}>
       <Paper
         sx={{
           bgcolor: "secondary.main",
@@ -15,12 +59,77 @@ export default function ETransferPage() {
           px: 4,
           pb: 4,
           borderRadius: 3,
-          height:"100%"
+          height: "100%",
         }}
       >
         <Title title="Send e-Transfer" />
-        <VerticalLinearStepper/>
+        <Grid container spacing={2}>
+          <Grid xs={12} lg={2}>
+            <Stepper
+              activeStep={activeStep}
+              orientation="vertical"
+              sx={{ display: { xs: "none", lg: "flex" } }}
+            >
+              {steps.map((step, index) => (
+                <Step key={step.label}>
+                  <StepLabel>{step.label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+            <Stepper
+              alternativeLabel
+              activeStep={activeStep}
+              orientation="horizontal"
+              sx={{ display: { xs: "flex", lg: "none" }, mb: 3 }}
+            >
+              {steps.map((step, index) => (
+                <Step key={step.label}>
+                  <StepLabel>{step.label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+          </Grid>
+
+          <Grid xs={12} md={12} lg={activeStep > 1 ? 10 : 7}>
+            {activeStep === steps.length - 1 && <ETransferSuccess handleReset={handleReset}/>}
+            {activeStep == 0 && <SelectAccountPayee />}
+            {activeStep == 1 && <EnterAmount />}
+            {activeStep == 2 && <Review />}
+            {activeStep <= steps.length - 2 && (
+              <Box sx={{ mb: 2 }}>
+                {activeStep > 0 && (
+                  <Button
+                    variant="outlined"
+                    onClick={handleBack}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    Back
+                  </Button>
+                )}
+                <Button
+                  variant="contained"
+                  onClick={handleNext}
+                  sx={{ mt: 1, mr: 1, px: 10 }}
+                >
+                  {activeStep === steps.length - 2 ? "Send Money" : "NEXT"}
+                </Button>
+              </Box>
+            )}
+          </Grid>
+          {activeStep < 2 && (
+            <Grid xs={12} md={4} lg={3}>
+              <Typography variant="h6" mb={1}>
+                From
+              </Typography>
+              <UserInfoCard />
+              <Typography variant="h6" mt={3} mb={1}>
+                To
+              </Typography>
+              <UserInfoCard />
+            </Grid>
+          )}
+        </Grid>
       </Paper>
     </Container>
-  )
+  );
 }
