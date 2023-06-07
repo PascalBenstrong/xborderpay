88ee9cb279dd6c6c29d50d7a1816d498b3817678
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-06-07 20:47:03 +0200

    improved auto complete component

--- app/components/x_autocomplete/index.tsx
+++ app/components/x_autocomplete/index.tsx
@@ -1,42 +1,72 @@
-import {
-    Autocomplete,
-    Box,
-    TextField,
-  } from '@mui/material';
-  
-  export default function XAutocomplete(props: any) {
-    let {value, setValue, mt,data} = props;
-  
-    return (
-      <Autocomplete
-          id="country-select-demo"
-          fullWidth
-          sx={{mt: mt ? mt : 3}}
-          options={data}
-          size="small"
-          clearOnBlur
-          inputValue={value}
-          onInputChange={(event, newInputValue,reason) => {
-            setValue(newInputValue);
+import { Autocomplete, Box, TextField } from "@mui/material";
+
+export default function XAutocomplete({
+  value,
+  setValue,
+  mt,
+  data,
+  id,
+}: {
+  value?: any;
+  setValue?: any;
+  mt?: number;
+  data: any;
+  id?: string;
+}) {
+  const handleChange = (
+    event: React.SyntheticEvent,
+    value: string,
+    reason: any
+  ) => {
+    setValue(value);
+  };
+
+  return (
+    <Autocomplete
+      id={id}
+      fullWidth
+      sx={{
+        mt: mt ? mt : 3,
+      }}
+      options={data}
+      size="medium"
+      clearOnBlur
+      inputValue={value}
+      onInputChange={handleChange}
+      getOptionLabel={(option: any) =>
+        option?.label?.length > 0 ? option.label : option
+      }
+      ListboxProps={{
+        sx: {
+          backgroundColor: "background.default",
+        },
+      }}
+      renderOption={(props, option: any) => (
+        <Box
+          component="li"
+          sx={{
+            "& > img": { mr: 2, flexShrink: 0 },
+            bgcolor: "secondary.main",
           }}
-          getOptionLabel={(option:any) => option.label}
-          renderOption={(props, option:any) => (
-          <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0},bgcolor: "secondary.main"}} {...props}>
-              
-              {option.label}
-          </Box>
-        )}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            sx={{bgcolor: "secondary.main", borderRadius: 1, border: '1px solid #ced4da'}}
-            inputProps={{
-              ...params.inputProps,
-              autoComplete: 'new-password', // disable autocomplete and autofill
-  
-            }}
-          />
-        )}
-      />
-    );
-  }
+          {...props}
+        >
+          {option?.label?.length > 0 ? option.label : option}
+        </Box>
+      )}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          sx={{
+            bgcolor: "secondary.main",
+            borderRadius: 1,
+            border: "1px solid #ced4da",
+          }}
+          inputProps={{
+            ...params.inputProps,
+            autoComplete: "new-password", // disable autocomplete and autofill
+          }}
+        />
+      )}
+    />
+  );
+}
