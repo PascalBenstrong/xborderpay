55e20eb680f691487fd82f0c99b4fc41ef54d9b2
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-06-07 21:33:18 +0200

    improved text color contrast for the card texts

--- app/components/x_autocomplete/index.tsx
+++ app/components/x_autocomplete/index.tsx
@@ -63,7 +63,6 @@ export default function XAutocomplete({
           }}
           inputProps={{
             ...params.inputProps,
-            autoComplete: "new-password", // disable autocomplete and autofill
           }}
         />
       )}
