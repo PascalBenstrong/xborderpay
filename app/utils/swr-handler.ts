d166407f8ac15665cf5d9f5a77da5913735f4dac
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-06-08 16:44:57 +0200

    updated swr handler to not fetch once

--- app/utils/swr-handler.ts
+++ app/utils/swr-handler.ts
@@ -5,11 +5,23 @@ const fetcher = (url: string) => {
 
     return fetch(url).then((res) => res.json());
 }
-export function useFetcher(url: string, token?: string, onSuccess?: (data:any) => void) {
+export function useFetcher(url: string, dataKey?: string, token?: string, onSuccess?: (data: any) => void) {
+    /* const revalidationOptions = {
+        revalidateOnMount: !Cache.has(dataKey), //here we refer to the SWR cache
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+      }; */
 
-    const { data, error, isLoading } = useSWR(url, fetcher,{onSuccess: (data,key,config)=>{
-        onSuccess != null && onSuccess(data?.data);
-    }});
+    const { data, error, isLoading } = useSWR(url, fetcher, {
+        revalidateOnFocus: false,
+        revalidateOnMount: false,
+        revalidateOnReconnect: false,
+        refreshWhenOffline: false,
+        refreshWhenHidden: false,
+        refreshInterval: 0, onSuccess: (data, key, config) => {
+            onSuccess != null && onSuccess(data?.data);
+        }
+    });
 
     return {
         data,
