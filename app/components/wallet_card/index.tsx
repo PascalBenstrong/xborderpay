bbb4fcf39a2757a9565d7fcf3fb2d84d0422d7bb
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-06-19 13:53:38 +0200

    simpilied wallet section

--- app/components/wallet_card/index.tsx
+++ app/components/wallet_card/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Paper, Avatar, Typography, Button } from '@mui/material'
+import React from "react";
+import { Paper, Avatar, Typography, Button } from "@mui/material";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 
-export default function WalletCard({name,currency, balance}:any) {
+export default function WalletCard({ name, currency, balance }: any) {
   return (
     <Paper sx={{ bgcolor: "secondary.main", p: 2 }}>
-                <Avatar sx={{ bgcolor: "primary.main" }} aria-label="currency">
-                  {currency}
-                </Avatar>
-                <Typography variant="h6" fontWeight={700} mt={2}>
-                  {`${currency} ${balance}`}
-                </Typography>
-                <Typography variant="body2" my={2}>
-                  {name}
-                </Typography>
-                <Button startIcon={<AddCircleIcon />}>Add Funds</Button>
-              </Paper>
-  )
+      <Avatar sx={{ bgcolor: "primary.main" }} aria-label="currency" >
+        {currency}
+      </Avatar>
+      <Typography variant="h6" fontWeight={700} mt={2}>
+        {`${currency} ${balance}`}
+      </Typography>
+      <Typography variant="body2" my={2}>
+        {name}
+      </Typography>
+      <Button startIcon={<AddCircleIcon />}>Add Funds</Button>
+    </Paper>
+  );
 }
