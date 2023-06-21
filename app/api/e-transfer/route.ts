7fea6bd4277f606cd3446d6b52d30fb61a38f3a9
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-06-21 07:15:45 +0200

    updated etransfer user info

--- app/api/e-transfer/route.ts
+++ app/api/e-transfer/route.ts
@@ -1,82 +1,43 @@
 import { NextResponse } from "next/server";
-import { User, Wallet } from "../../types";
+import { Currency, User, Wallet } from "../../types";
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
 
     const accounts: Wallet[] = [
         {
-            id: "1",
-            name: "Euros",
-            currency: "EUR",
-            balance: 115741,
-            logo: "",
+            userId: "648f2dc74469b5d1e702451e",
+            currency: Currency.USD,
+            name: "USD",
+            account: {
+                id: "0.0.14826128",
+                publicKey: "302a300506032b657003210080274d459fcbda0e569f4c250bdc3ad632832947e081eb18817052257b3c67fc",
+                type: "hedera"
+            },
+            balance: 500,
+            id: "648f2dc94469b5d1e702451f"
         },
         {
-            id: "2",
-            name: "US dollar",
-            currency: "USD",
-            balance: 1005,
-            logo: "",
-        },
-        {
-            id: "3",
-            name: "South African Rand",
-            currency: "ZAR",
-            balance: 39134,
-            logo: "",
-        },
+            userId: "648f2dc74469b5d1e702451e",
+            name: "ZAR Wallet",
+            currency: Currency.ZAR,
+            account: {
+                id: "0.0.14827473",
+                publicKey: "302a300506032b65700321004f256f5baa993fcaf5dc623a2d4f98ec4e477cb8cfe1d77fd95280225abdfcca",
+                type: "hedera"
+            },
+            balance: 2000,
+            id: "648f9ea5ef1d4e69bf3d7dbd"
+        }
     ];
 
     const user = {
-        name: "Mark Woods",
-            email: "mark.wood@gmail.com",
-            phone: "+27 80 000 0000",
-            address: "22 Bree Street, Cape Town, Western Cape, South Africa",
-            birthdate: "1966-08-22",
-            fiId: "004-12345",
-            accountNo: "123456789012",
-            bankName: "ABSA Bank"
+        firstName: "Mark",
+        lastName: "Woods",
+        email: "mark.wood@gmail.com",
+        wallets: accounts,
     }
 
-    const recentPayees: User[] = [
-          {
-            id: "1",
-            firstName: "John",
-            lastName: "Doe",
-            email: "john.doe@gmail.com",
-            phone: "+27 81 111 1111",
-            address: "15 Main Street, Johannesburg, Gauteng, South Africa",
-            birthdate: "1978-05-10",
-            fiId: "004-67890",
-            accountNo: "987654321098",
-            bankName: "Standard Bank"
-          },
-          {
-            id: "2",
-            firstName: "Jane",
-            lastName: "Smith",
-            email: "jane.smith@gmail.com",
-            phone: "+27 82 222 2222",
-            address: "10 High Road, Durban, KwaZulu-Natal, South Africa",
-            birthdate: "1990-11-15",
-            fiId: "004-54321",
-            accountNo: "456789012345",
-            bankName: "First National Bank"
-          },
-          {
-            id: "1",
-            firstName: "Sarah",
-            lastName: "Johnson",
-            email: "sarah.johnson@gmail.com",
-            phone: "+27 83 333 3333",
-            address: "5 Elm Avenue, Pretoria, Gauteng, South Africa",
-            birthdate: "1985-07-03",
-            fiId: "004-98765",
-            accountNo: "654321098765",
-            bankName: "Nedbank"
-          }
-    ];
 
     const purposes: string[] = [
         "Software development services",
@@ -88,7 +49,7 @@ export async function GET(request: Request) {
 
     const data = {
         accounts,
-        recentPayees,
+        user,
         purposes
     }
 
